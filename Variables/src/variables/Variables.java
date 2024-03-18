69cc7a84d23d454ce026cf24e09cbb44e483fb68
mcepulis <109752570+mcepulis@users.noreply.github.com>
2024-03-18 13:37:31 +0200

    update

--- Variables/src/variables/Variables.java
+++
@@ -1,9 +0,0 @@
-package variables;
-
-public class Variables {
-    public static void main(String[] args) {
-        float x = 5;
-        float y = 8;
-        System.out.println(x/y);
-    }
-}
---
+++ Variables/src/variables/VariablesInt.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class VariablesInt {
+    public static void main(String[] args) {
+        float x = -5.5f;
+        x = 5.6f;
+        float y = 8.1f;
+        float rez = (x/y);
+         System.out.println(rez);
+         System.out.println(x);
+    }
+}
+
